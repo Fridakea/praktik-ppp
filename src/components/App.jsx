7e07c76cccfaf8c3cb8977a6ce66a239eb0dfbcc
Fridakea <frbe0002@stud.kea.dk>
2024-10-31 21:17:53 +0100

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ export function App() {
 
   return (
     <div id='app' className={isDarkMode ? 'dark-mode' : ''}>
-      <main className="App max-w-[1150px] mx-auto px-4">
+      <main className="App max-w-[1200px] mx-auto px-4">
         <DarkmodeToggle />
         <HeroSection />
         <AboutMe />
